a67fca5f8de5939a4fe521a5b26f3da9a20443de
Samuel <Samuel@192.168.1.5>
2019-11-13 19:25:42 +0000

    1

--- src/uk/ac/ed/bikerental/DefaultPricingPolicy.java
+++ src/uk/ac/ed/bikerental/DefaultPricingPolicy.java
@@ -2,19 +2,29 @@ package uk.ac.ed.bikerental;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.Hashtable;
 
 public class DefaultPricingPolicy implements PricingPolicy{
+    
+    private Hashtable<BikeType,BigDecimal> pricingList;
 
     @Override
     public void setDailyRentalPrice(BikeType bikeType, BigDecimal dailyPrice) {
-        // TODO Auto-generated method stub
-        
+        pricingList.put(bikeType, dailyPrice);
     }
 
     @Override
     public BigDecimal calculatePrice(Collection<Bike> bikes, DateRange duration) {
-        // TODO Auto-generated method stub
-        return null;
+        BigDecimal totalPrice = new BigDecimal("0");
+        BigDecimal numberOfDays = new BigDecimal(duration.toDays());
+        
+        for (Bike bike : bikes) {
+            BikeType type = bike.getType(); 
+            BigDecimal dailyPrice = pricingList.get(type);
+            totalPrice.add(dailyPrice.multiply(numberOfDays));
+            
+        }
+        return totalPrice;
     }
 
 }
