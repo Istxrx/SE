22c9e4c113d23b35d24bb01c7709eb619429154d
Emma <Emma@192.168.1.6>
2019-11-13 17:50:31 +0000

    s

--- src/uk/ac/ed/bikerental/LinearDepreciation.java
+++ src/uk/ac/ed/bikerental/LinearDepreciation.java
@@ -4,8 +4,30 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class LinearDepreciation implements ValuationPolicy {
+    private BigDecimal depreciationRate;
+    private BigDecimal replacementValue;
+    
+    public LinearDepreciation
+    (BigDecimal replacementValue, BigDecimal depreciationRate) {
+        this.depreciationRate = depreciationRate;
+        this.replacementValue = replacementValue;
+    }
+
+    public void setDepreciationRate(BigDecimal depreciationRate) {
+        this.depreciationRate = depreciationRate;
+    }
+
+
+    public void setReplacementValue(BigDecimal replacementValue) {
+        this.replacementValue = replacementValue;
+    }
+
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
-        return null;
+        BigDecimal age = new BigDecimal (new DateRange(bike.getAge(),date).toYears());
+        
+        return 
+        replacementValue.subtract((age.multiply(depreciationRate)).multiply(replacementValue));
         
     };
 }
+
