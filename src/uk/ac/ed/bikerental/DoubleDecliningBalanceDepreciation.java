2d2521d2523e1fdd111e29080a0eab4ea0c0a684
Emma <Emma@192.168.1.6>
2019-11-13 17:12:43 +0000

    gf

--- src/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciation.java
+++ src/uk/ac/ed/bikerental/DoubleDecliningBalanceDepreciation.java
@@ -9,8 +9,26 @@ public class DoubleDecliningBalanceDepreciation implements ValuationPolicy  {
     private BigDecimal replacementValue;
     private long age;
     
+    public DoubleDecliningBalanceDepreciation
+    (BigDecimal replacementValue, BigDecimal depreciationRate, long age) {
+        this.depreciationRate = depreciationRate;
+        this.replacementValue = replacementValue;
+        this.age = age;
+    }
+
+    public void setDepreciationRate(BigDecimal depreciationRate) {
+        this.depreciationRate = depreciationRate;
+    }
+
+
+    public void setReplacementValue(BigDecimal replacementValue) {
+        this.replacementValue = replacementValue;
+    }
+
     public BigDecimal calculateValue(Bike bike, LocalDate date) {
         
+        return (replacementValue (new BigDecimal("1").subtract
+                (new BigDecimal(age.toString())depreciationRate))
         
     };
 }
