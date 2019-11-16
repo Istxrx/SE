c291887963500c219cddde82cf90beb4b4d057fc
Emma <Emma@192.168.1.6>
2019-11-16 17:30:27 +0000

    doc

--- src/uk/ac/ed/bikerental/Location.java
+++ src/uk/ac/ed/bikerental/Location.java
@@ -1,20 +1,28 @@
 package uk.ac.ed.bikerental;
 
 /**
- * This class is used to encapsulate post code and address as its String attributes.
+ * This class is used to encapsulate post code and address as its {@link String} attributes.
  */
 
 public class Location {
 
+/**
+ * This is post code.    
+ */
     private String postcode;
+    
+/**
+ * This is address.    
+ */
+    
     private String address;
     
 /**
- * Creates new object Location with post code and address as String attributes.
+ * Creates new object Location with post code and address as {@link String} attributes.
  *   
- * @param postcode stores post code of the location as String
+ * @param postcode stores post code of the location as {@link String}
  *        <b>Note:</b> postcode.length() must be >=6
- * @param address stores address of the location as String
+ * @param address stores address of the location as {@link String}
  */ 
     public Location(String postcode, String address) {
         assert postcode.length() >= 6;
@@ -43,14 +51,14 @@ public class Location {
     }
     
     /**
-     * @return this.postcode as String
+     * @return this.postcode as {@link String}
      */
     public String getPostcode() {
         return this.postcode;
     }
     
     /**
-     * @return this.address as String
+     * @return this.address as {@link String}
      */
     public String getAddress() {
         return this.address;
