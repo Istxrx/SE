90e558b65b3d74462307f46c2c58ed564537e171
Samuel <Samuel@192.168.1.5>
2019-11-16 14:53:21 +0000

    1

--- src/uk/ac/ed/bikerental/Provider.java
+++ src/uk/ac/ed/bikerental/Provider.java
@@ -1,5 +1,43 @@
 package uk.ac.ed.bikerental;
 
+import java.util.Collection;
+import java.util.Objects;
+
 public class Provider {
+    
+    private String name;
+    private Integer ID;
+    private Location address;
+    private String phoneNumber;
+    private Collection<Provider> partners;
+    private Collection<Bike> bikes;
+    
+    public Provider(String name, Integer iD, Location address, String phoneNumber, 
+            Collection<Provider> partners,Collection<Bike> bikes) {
+        
+        this.name = name;
+        this.ID = iD;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.partners = partners;
+        this.bikes = bikes;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        return this.ID.equals(((Provider) other).getID());
+    }
+   
+    @Override
+    public int hashCode() {
+        // hashCode method allowing use in collections
+        return Objects.hash(this.ID);
+    }
+    
+     
+    private Object getID() {
+        return this.ID;
+    }
+
 
 }
