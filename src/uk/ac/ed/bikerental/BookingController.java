85fffbf20d7a697b6319193f086473a1402985c3
SmejoTT <adrian.trajlinek@gmail.com>
2020-05-13 16:21:53 +0100

    update

--- src/uk/ac/ed/bikerental/BookingController.java
+++ src/uk/ac/ed/bikerental/BookingController.java
@@ -8,7 +8,7 @@ public class BookingController {
 
     private Collection<Booking>allBookings;
     private ProviderController providerController;
-    // "temporary development measure" to ensure uniqueness of IDs
+    // "temporary measure" to ensure uniqueness of IDs
     private Integer IdCounter;
     
     
